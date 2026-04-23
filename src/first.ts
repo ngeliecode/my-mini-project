228eb3930a797ee29585d7f362da653a1afe994b
Angelie <angelie.angman@gmail.com>
2026-04-23 19:39:51 +0200

    ✨ feat: add morning sky on click

--- src/first.ts
+++ src/first.ts
@@ -1,18 +1,24 @@
 export function showSecondPage() {
   const enterBtn = document.querySelector("#enter") as HTMLButtonElement;
+
   const sun = document.querySelector(".sun") as HTMLDivElement;
+  const sky = document.querySelector(".sky") as HTMLDivElement;
+
   const firstPage = document.querySelector("#firstPage") as HTMLElement;
   const secondPage = document.querySelector("#secondPage") as HTMLElement;
 
-  if (enterBtn && sun) {
+  if (enterBtn && sun && sky) {
     console.log("Enter button found");
 
     enterBtn.addEventListener("click", () => {
-      firstPage.classList.add("hidden");
-      secondPage.classList.remove("hidden");
-
-      // animera solen
+      // animation
       sun.classList.add("rise");
+      sky.classList.add("morning");
+
+      setTimeout(() => {
+        firstPage.classList.add("hidden");
+        secondPage.classList.remove("hidden");
+      }, 2000); // Match the sun's transition duration
     });
   }
 }
