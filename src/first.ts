f1f83ce7db07f78c509dab8d818a6428b428e50e
Angelie <angelie.angman@gmail.com>
2026-04-23 11:20:55 +0200

    ✨ feat: add sunrise on click

--- src/first.ts
+++ src/first.ts
@@ -1,13 +1,18 @@
 export function showSecondPage() {
   const enterBtn = document.querySelector("#enter") as HTMLButtonElement;
+  const sun = document.querySelector(".sun") as HTMLDivElement;
   const firstPage = document.querySelector("#firstPage") as HTMLElement;
   const secondPage = document.querySelector("#secondPage") as HTMLElement;
 
-  if (enterBtn) {
+  if (enterBtn && sun) {
     console.log("Enter button found");
+
     enterBtn.addEventListener("click", () => {
       firstPage.classList.add("hidden");
       secondPage.classList.remove("hidden");
+
+      // animera solen
+      sun.classList.add("rise");
     });
   }
 }
